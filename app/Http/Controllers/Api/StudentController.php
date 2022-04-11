7ad7abaec6365c73ed1013894ba0716663eb03c6
Rafal Kadzevic <kadzewiczrafal@gmail.com>
2022-04-11 19:03:22 +0300

    Added RestApi to add new student

---
+++ app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StudentStoreRequest;
+use App\Http\Resources\StudentResource;
+use App\Models\Project;
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return StudentResource::collection(Student::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $project = Project::first();
+        if($project){
+            $this->validate($request,[
+                'full_name' => 'required|alpha_spaces|min:5|unique:students'
+            ]);
+            $new_student = Student::create($request->all());
+            return $new_student;
+        }else{
+            return response()->json([
+             'Message' => 'Project is not set!',
+            ],422);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+//        return new StudentResource(Student::with('group')->findOrFail($id));
+        return new StudentResource(Student::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
